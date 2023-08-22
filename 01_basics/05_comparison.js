f0eeaf3de5685f2cbf3b49a7969d29deb394f2f9
Abhimanyu Singh <112697432+AbhimanyuSingh2005@users.noreply.github.com>
2023-08-22 16:52:12 +0000

    learned operations and comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);//true
+// console.log(2<1);//false
+// console.log(2<=1);//false
+// console.log(2>=1);//true
+// console.log(2==2);//true
+
+//comparison operate first change the value of null to 0 and then compare
+// whereas equality operator does not do it
+// console.log(null > 0 );//false
+// console.log(null >= 0);//true
+// console.log(null == 0);//false
+
+// console.log('10'>1);//true
+// console.log('10'<1);//false
+// console.log('10'==1);//false
+
+console.log(undefined > 0 );//false
+console.log(undefined >= 0);//false
+console.log(undefined == 0);//false
+
+// === strict equality check your datatype with value 
