18170a278b2a369b8915d2dc35f8c3220468c30c
Abhimanyu Singh <112697432+AbhimanyuSingh2005@users.noreply.github.com>
2023-08-24 11:38:38 +0000

    learned dates in js

---
+++ 01_basics/08_datesInJS.js
@@ -0,0 +1,20 @@
+const date = new Date();
+
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toUTCString());
+
+// let createdDate = new Date(2023,0,20);
+// let createdDate = new Date(2023,0,20,15,24);
+let createdDate = new Date("01-01-2023");
+
+
+
+
+console.log(createdDate.toLocaleString());
