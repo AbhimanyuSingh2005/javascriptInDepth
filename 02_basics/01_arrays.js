cbbf5f97032d439726e74dffdeab43dcd66ac340
Abhimanyu Singh <112697432+AbhimanyuSingh2005@users.noreply.github.com>
2023-08-24 12:13:13 +0000

    leearned something about array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+const arr =  [1,2,3,4,5];
+const myHeroes = ['Ironman','Hulk','thor','Captain America'];
+const myArr = new Array(0,1,2,3,4,5);
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(0)
+// myArr.shift()
+
+// console.log(myArr.indexOf(4));
+
+const myn1 = myArr.slice(1,4);
+console.log(myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,4);
+console.log(myArr);
+console.log(myn2);
