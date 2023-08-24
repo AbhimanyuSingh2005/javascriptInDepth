fdc8cf026bc4c31ca4a9a6654f0d132edf46d4fb
Abhimanyu Singh <112697432+AbhimanyuSingh2005@users.noreply.github.com>
2023-08-24 07:56:13 +0000

    learned strings and number

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,10 @@
+const name = 'Abhimanyu Singh';
+const repoCount = 40;
+// console.log(`My Name is ${name} and my repo count is ${repoCount}`); 
+
+// console.log(name.toUpperCase())
+// console.log(name.charAt(4));
+// console.log(name.indexOf('u'));
+
+// to my future seelf there is note :
+// Saare protype function ek bar practice kar lio
---
+++ 01_basics/07_numbersAndMaths.js
@@ -0,0 +1,21 @@
+const num = 100;
+
+const n = new Number (200);
+
+// console.log(num);
+// console.log(n);
+
+// Note for my future self 
+// Number ke sare prototype dekh lo
+
+///////////////////////////////////     math          ////////////////////
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+const min = 10 ;
+const max = 20 ;
+
+console.log(Math.floor(Math.random()*(max-min + 1))+min);
